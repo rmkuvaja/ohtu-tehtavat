c454ba3b30065a459b79bef828f703f8cd4bacc2
Riina Zonzi <riina.zonzi@helsinki.fi>
2022-03-16 13:20:10 +0200

    viikko 3 tehtävä 1

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,34 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+
+#    print("JSON-muotoinen vastaus:")
+#    print(response)
+
+    players = []
+    nationality = 'FIN'
+
+    for player_dict in response:
+        if player_dict['nationality'] == nationality:
+            name = player_dict['name']
+            team = player_dict['team']
+            goals = player_dict['goals']
+            assists = player_dict['assists']
+            player = Player(
+                 name + "team " + team + "  goals " + str(goals)  + "  assists " + str(assists)
+            )
+            players.append(player)
+
+
+    print("Players from FIN:")
+
+    for player in players:
+        print(player)
+
 
 if __name__ == "__main__":
     main()
+
